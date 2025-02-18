d7e8287d89e6d6e5963799382d3c94620e8e7202
Joao <joaovr.og@gmail.com>
2025-02-17 21:46:53 -0300

    adding initial files for project

---
+++ src/main/java/com/joss/dblib/DblibApplication.java
@@ -0,0 +1,13 @@
+package com.joss.dblib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DblibApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DblibApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joss/dblib/DblibApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joss.dblib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DblibApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
