d006ec6f324177c361db9d620e5623c59f7d78ee
Joao <joaovr.og@gmail.com>
2025-02-18 23:39:42 -0300

    adding DatabaseConfig class and (scratch) dataSource bean

---
+++ src/main/java/config/DatabaseConfig.java
@@ -0,0 +1,45 @@
+package config;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableTransactionManagement
+@EnableJpaRepositories(basePackages = {"${database.config.base}"})
+public class DatabaseConfig {
+
+    private static final Logger log = LoggerFactory.getLogger(DatabaseConfig.class);
+    //Todo: some database properties class?
+    //Todo: database properties injection
+
+
+    @Bean(name = {"dataSource"})
+    public DataSource dataSource() {
+        log.info("Starting dataSource()");
+        HikariConfig hc = new HikariConfig();
+        hc.setDriverClassName(null); //todo: field from database properties class
+        hc.setJdbcUrl(null); //todo: field from database properties class
+        hc.setUsername(null); //todo: field from database properties class
+        hc.setPassword(null); //todo: field from database properties class
+        hc.setPoolName(null); //todo: field from database properties class
+        hc.setMinimumIdle(0); //todo: field from database properties class
+        hc.setMaximumPoolSize(0); //todo: field from database properties class
+        hc.setMaxLifetime(0); //todo: field from database properties class
+        hc.setValidationTimeout(0L); //todo: field from database properties class
+        hc.setSchema(null); //todo: field from database properties class
+
+        return new HikariDataSource(hc);
+    }
+    
+
+
+
+}
